98523ea322ff01527aa47e8ec84cfeb1a05dfff2
Jesse Falconer <jesse.falconer@spark.re>
2020-12-13 20:42:17 -0800

    Begin exports

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Shipment < ApplicationRecord
+  require "csv"
+
   STATUSES = ["Not Started", "In Progress", "Ready to Ship", "In Transit", "Received"].freeze
 
   belongs_to :user, optional: false
@@ -16,4 +18,21 @@ class Shipment < ApplicationRecord
   validates :status, inclusion: { in: STATUSES }
 
   paginates_per 10
+
+  def to_csv
+    attributes = %w[id name status custom_uid]
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      csv << ["PALLETS"]
+      pallets.each do |pallet|
+        csv << attributes.map{ |attr| pallet.send(attr) }
+        csv << ["BOXES"]
+        pallet.boxes.each do |box|
+          csv << attributes.map{ |attr| box.send(attr) }
+        end
+      end
+    end
+  end
 end
