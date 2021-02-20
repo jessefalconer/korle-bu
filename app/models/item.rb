3caf88910e5ef5b9d8798781c8b2b86a5382d6d6
Jesse Falconer <jesse.falconer@spark.re>
2021-02-19 23:18:57 -0800

    Add sizes Newborn and Preemie

--- app/models/item.rb
+++ app/models/item.rb
@@ -30,8 +30,8 @@ class Item < ApplicationRecord
   validates :generated_name, uniqueness: true
 
   STANDARD_SIZES = %w[XXXS XXS XS Small Medium Large XL XXL XXXL].freeze
-  INFANT_SIZES = %w[Infant Infant\ Size\ 1 Infant\ Size\ 2 Infant\ Size\ 2.5
-                   Infant\ Size\ 3 Infant\ Size\ 3.5 Infant\ Size\ 4 Infant\ Size\ 4.5] +
+  INFANT_SIZES = %w[Infant Newborn Preemie Infant\ Size\ 1 Infant\ Size\ 2 Infant\ Size\ 2.5
+                   Infant\ Size\ 3 Infant\ Size\ 3.5 Infant\ Size\ 4 Infant\ Size\ 4.5 Infant\ Size\ 5 Infant\ Size\ 6 Infant\ Size\ 7] +
                    STANDARD_SIZES.map { |s| "Infant #{s}" }.freeze
   CHILD_SIZES = %w[Child] + STANDARD_SIZES.map { |s| "Child #{s}" }.freeze
   ADULT_SIZES = %w[Adult] + STANDARD_SIZES.map { |s| "Adult #{s}" }.freeze
@@ -53,7 +53,7 @@ class Item < ApplicationRecord
     ["Infant", INFANT_SIZES],
     ["Child", CHILD_SIZES],
     ["Adult", ADULT_SIZES]
-  ]
+  ].freeze
 
   before_validation do
     sanitize_whitespace
