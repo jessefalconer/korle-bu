e05ce1226c4a2df3a4b2b2cfe76f758553ac32b1
Jesse Falconer <jesse.falconer@spark.re>
2021-11-07 14:36:25 -0800

    Show recently unpacked items in admin panel

--- app/models/unpacking_event.rb
+++ app/models/unpacking_event.rb
@@ -5,7 +5,7 @@ class UnpackingEvent < ApplicationRecord
   belongs_to :user, optional: false
   belongs_to :hospital, optional: false
 
-  delegate :generated_name, to: :packed_item
+  delegate :generated_name, :item, to: :packed_item
 
   after_commit do
     packed_item.save!
