f2cc2c98b52393696d2d7e90c2130b5b36eb8632
Jesse Falconer <jesse.falconer@spark.re>
2022-05-12 22:52:21 -0700

    Bug fix: unpack permission through pallet assoc

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -43,6 +43,7 @@ class Ability
       can :find, Pallet, shipment: { receiving_warehouse_id: user.warehouse_id }, status: Pallet::RECEIVED
       can :read, Box, container: { shipment: { receiving_warehouse_id: user.warehouse_id } }, status: Box::RECEIVED
       can :unpack_all, Box, container: { shipment: { receiving_warehouse_id: user.warehouse_id } }, status: Box::RECEIVED
+      can :unpack_all, Box, pallet: { container: { shipment: { receiving_warehouse_id: user.warehouse_id } } }, status: Box::RECEIVED
       can :find, Box, container: { shipment: { receiving_warehouse_id: user.warehouse_id } }, status: Box::RECEIVED
       can :read, Box, pallet: { container: { shipment: { receiving_warehouse_id: user.warehouse_id } } }, status: Box::RECEIVED
       can :manage, UnpackingEvent
